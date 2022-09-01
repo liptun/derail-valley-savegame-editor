9a6f09e4aa821e48124b54ec261ecf36a2e86ecc
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2022-09-01 23:33:04 +0200

    Cleanup

--- src/renderer/Components/App.tsx
+++ src/renderer/Components/App.tsx
@@ -1,6 +1,5 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 import { savegameDecodeToJSON } from "../../utils/savegame";
-import styled from "styled-components";
 import { ElectronAPI } from "../../preload";
 import ReactJson from "react-json-view";
 
@@ -10,31 +9,25 @@ declare global {
     }
 }
 
-const SavegameRAW = styled.textarea`
-    display: block;
-    width: 100%;
-    min-height: 300px;
-`;
-const SavegameJSON = styled(SavegameRAW)``;
-
 const App: FC = () => {
-    const [savegame, setSavegame] = useState("");
+    const [isLoading, setLoading] = useState(false);
     const [savegameJSON, setSavegameJSON] = useState({});
+
     const onClickHandle = async () => {
+        setLoading(true);
         const fileContent = await window.electronAPI.openFile();
-        setSavegame(fileContent);
+        savegameDecodeToJSON(fileContent).then((v) => {
+            setLoading(false);
+            setSavegameJSON(JSON.parse(v));
+        });
     };
-    useEffect(() => {
-        savegameDecodeToJSON(savegame).then(
-            (v) => v && setSavegameJSON(JSON.parse(v))
-        );
-    }, [savegame]);
+
     return (
         <>
             <h1>Derail Valley</h1>
             <h2>Savegame Editor</h2>
             <button onClick={onClickHandle}>Open savegame</button>
-            <SavegameRAW value={savegame} />
+            {isLoading && <p>Loading...</p>}
             <ReactJson src={savegameJSON} />
         </>
     );
