b3f117da11532f066836e0af35658010fbc43835
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2022-09-04 10:42:53 +0200

    OpenFile component styles

--- src/renderer/Components/OpenFile.tsx
+++ src/renderer/Components/OpenFile.tsx
@@ -13,12 +13,30 @@ const Wrapper = styled.div`
 
 const Label = styled.p`
     text-align: center;
-`
+    margin-bottom: 30px;
+    font-size: 1.8em;
+`;
+
+const Hint = styled.p`
+    font-size: 0.8em;
+`;
+const Path = styled.code`
+    font-size: 0.8em;
+    font-family: monospace;
+    background-color: #ccc;
+    padding: 8px 12px;
+    color: #333;
+    border-radius: 8px;
+`;
 
 const OpenFile: FC = () => {
     return (
         <Wrapper>
             <Label>Load savegame file</Label>
+            <Hint>Derail Valley stores save data in this direction:</Hint>
+            <Path>
+                /steamapps/common/Derail Valley/DerailValley_Data/SaveGameData
+            </Path>
             <Button label="Open" />
         </Wrapper>
     );
